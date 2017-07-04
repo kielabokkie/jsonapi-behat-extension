3a2270648a98bd898319bc97e7d7fee9d2e610c2
Antonio De Marco <antonio@nuvole.org>
2017-07-04 16:38:45 +0200

    Add JsonApiAwareInterface so that contexts can opt-in.

--- src/Context/JsonApiContext.php
+++ src/Context/JsonApiContext.php
@@ -10,7 +10,7 @@ use PHPUnit_Framework_Assert as PHPUnit;
 /**
  * Defines application features from the specific context.
  */
-class JsonApiContext implements SnippetAcceptingContext
+class JsonApiContext implements SnippetAcceptingContext, JsonApiAwareInterface
 {
     /**
      * @var \Buzz\Browser
